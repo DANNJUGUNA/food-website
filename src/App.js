dd69e60b4e7fc56c420f8d44eae90296a9c31373
DANNJUGUNA <ndan30071@gmail.com>
2023-03-24 10:50:52 +0300

    imported component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import HeadlinesCards from "./components/HeadlinesCards";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
+      <HeadlinesCards/>
     </div>
   );
 }
